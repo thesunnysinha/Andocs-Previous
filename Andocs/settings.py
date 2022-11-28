b821579b1dde441ae53ba7c91c9740f7decaea8c
sunny kumar sinha <90440046+thesunnysinha@users.noreply.github.com>
2022-11-28 22:07:33 +0530

    Update settings.py

--- Andocs/settings.py
+++ Andocs/settings.py
@@ -22,7 +22,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q+!8f_d^7h!$=##2efciz2qm2n!-0x(=^wylfo(p^wh#oc0vae'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
